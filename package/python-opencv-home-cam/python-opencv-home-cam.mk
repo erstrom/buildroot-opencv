c6c49905235fc406926e427e4724190f71d73adb
Erik Stromdahl <erik.stromdahl@gmail.com>
2017-08-22 07:28:13 +0200

    package/python-opencv-home-cam: add detection config options
    Add possibility to select from different detectors.
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- package/python-opencv-home-cam/python-opencv-home-cam.mk
+++ package/python-opencv-home-cam/python-opencv-home-cam.mk
@@ -11,10 +11,39 @@ PYTHON_OPENCV_HOME_CAM_SETUP_TYPE = setuptools
 PYTHON_OPENCV_HOME_CAM_LICENSE = MIT
 PYTHON_OPENCV_HOME_CAM_DEPENDENCIES = python-imutils
 
+define PYTHON_OPENCV_HOME_CAM_SETUP_HAAR_CASCADE_DETECTOR
+	( \
+		mkdir -p $(TARGET_DIR)/etc/opencv-home-cam/haar-cascades; \
+		cp $(@D)/example-configs/config-haar-face-detection.ini \
+			$(TARGET_DIR)/etc/opencv-home-cam/config.ini; \
+		cp $(@D)/haar-cascades/haarcascade_frontalface_default.xml \
+			$(TARGET_DIR)/etc/opencv-home-cam/haar-cascades; \
+		sed -i s/cascade=.*/cascade=\\/etc\\/opencv-home-cam\\/haar-cascades\\/haarcascade_frontalface_default\.xml/ \
+			$(TARGET_DIR)/etc/opencv-home-cam/config.ini; \
+	)
+endef
+
+define PYTHON_OPENCV_HOME_CAM_SETUP_SIMPLE_MOTION_DETECTOR
+	( \
+		mkdir -p $(TARGET_DIR)/etc/opencv-home-cam; \
+		cp $(@D)/example-configs/config-simple-motion.ini \
+			$(TARGET_DIR)/etc/opencv-home-cam/config.ini; \
+	)
+endef
+
+ifeq ($(BR2_PACKAGE_PYTHON_OPENCV_HOME_CAM_HAAR_CASCADE_DETECTOR), y)
+define PYTHON_OPENCV_HOME_CAM_SETUP_DETECTOR
+	$(PYTHON_OPENCV_HOME_CAM_SETUP_HAAR_CASCADE_DETECTOR)
+endef
+else ifeq ($(BR2_PACKAGE_PYTHON_OPENCV_HOME_CAM_SIMPLE_MOTION_DETECTOR), y)
+define PYTHON_OPENCV_HOME_CAM_SETUP_DETECTOR
+	$(PYTHON_OPENCV_HOME_CAM_SETUP_SIMPLE_MOTION_DETECTOR)
+endef
+endif
+
 ifeq ($(BR2_PACKAGE_PYTHON_OPENCV_HOME_CAM_SEND_EMAIL_ACTION), y)
 define PYTHON_OPENCV_HOME_CAM_INSTALL_SENDEMAIL_ACTION
 	( \
-		mkdir -p $(TARGET_DIR)/etc/opencv-home-cam; \
 		cp $(@D)/example-actions/sendemail.sh $(TARGET_DIR)/etc/opencv-home-cam/sendemail.sh; \
 		sed -i s/FROM_ADDR=\".*\"/FROM_ADDR=\"$(BR2_PACKAGE_PYTHON_OPENCV_HOME_CAM_SEND_EMAIL_ACTION_FROM_ADDR)\"/ \
 			$(TARGET_DIR)/etc/opencv-home-cam/sendemail.sh; \
@@ -24,13 +53,6 @@ define PYTHON_OPENCV_HOME_CAM_INSTALL_SENDEMAIL_ACTION
 			$(TARGET_DIR)/etc/opencv-home-cam/sendemail.sh; \
 		sed -i s/SMTP_PASSWD=\".*\"/SMTP_PASSWD=\"$(BR2_PACKAGE_PYTHON_OPENCV_HOME_CAM_SEND_EMAIL_SMTP_PASSWD)\"/ \
 			$(TARGET_DIR)/etc/opencv-home-cam/sendemail.sh; \
-		mkdir -p $(TARGET_DIR)/etc/opencv-home-cam/haar-cascades; \
-		cp $(@D)/example-configs/config-haar-face-detection.ini \
-			$(TARGET_DIR)/etc/opencv-home-cam/config.ini; \
-		cp $(@D)/haar-cascades/haarcascade_frontalface_default.xml \
-			$(TARGET_DIR)/etc/opencv-home-cam/haar-cascades; \
-		sed -i s/cascade=.*/cascade=\\/etc\\/opencv-home-cam\\/haar-cascades\\/haarcascade_frontalface_default\.xml/ \
-			$(TARGET_DIR)/etc/opencv-home-cam/config.ini; \
 		sed -i s/command=.*/command=\\/etc\\/opencv-home-cam\\/sendemail\.sh/ \
 			$(TARGET_DIR)/etc/opencv-home-cam/config.ini; \
 		sed -i s/recording_dir=.*/recording_dir=\\/root\\/opencv-home-cam-recordings/ \
@@ -53,6 +75,7 @@ endef
 
 define PYTHON_OPENCV_HOME_CAM_INSTALL_EXTRA
 	$(PYTHON_OPENCV_HOME_CAM_INSTALL_LOGGING_CFG)
+	$(PYTHON_OPENCV_HOME_CAM_SETUP_DETECTOR)
 	$(PYTHON_OPENCV_HOME_CAM_INSTALL_SENDEMAIL_ACTION)
 endef
 
