0970ba9fc848546adbb7224c1b9a5c99e8c9db1a
Erik Stromdahl <erik.stromdahl@gmail.com>
2017-09-01 14:08:24 +0200

    package/python-opencv-home-cam: bump revision
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- package/python-opencv-home-cam/python-opencv-home-cam.mk
+++ package/python-opencv-home-cam/python-opencv-home-cam.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_OPENCV_HOME_CAM_VERSION = 7eab4c55dbe424d612418c824bf9316ce5f384e2
+PYTHON_OPENCV_HOME_CAM_VERSION = 866ce112deb3399702913ff5468a0cadcc82090f
 PYTHON_OPENCV_HOME_CAM_SOURCE = opencv-home-cam-$(PYTHON_OPENCV_HOME_CAM_VERSION).tar.gz
 PYTHON_OPENCV_HOME_CAM_SITE = $(call github,erstrom,opencv-home-cam,$(PYTHON_OPENCV_HOME_CAM_VERSION))
 PYTHON_OPENCV_HOME_CAM_SETUP_TYPE = setuptools
